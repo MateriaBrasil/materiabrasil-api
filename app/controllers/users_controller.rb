007b8afa8c6b1c2766f62bffb66eb98477a42124
Daniel Weinmann <danielweinmann@gmail.com>
2018-07-22 21:39:01 -0300

    Remove custom message for profile not public

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,10 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  rescue_from Pundit::NotAuthorizedError, with: :profile_not_public
-
   def show
     user = User.find(params[:id])
     authorize user
 
     render json: user
-  end
-
-  private
-
-  def profile_not_public
-    render status: :forbidden, json: {
-      id: 'forbidden',
-      message: 'Perfil privado'
-    }
   end
 end
