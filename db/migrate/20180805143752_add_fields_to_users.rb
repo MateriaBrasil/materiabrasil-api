b2844df53def94e6b6e1055fc08cfc34b32a14e6
felipefreitag <ffvargas@gmail.com>
2018-08-05 13:58:42 -0300

    Fix migration

--- db/migrate/20180805143752_add_fields_to_users.rb
+++ db/migrate/20180805143752_add_fields_to_users.rb
@@ -2,9 +2,11 @@
 
 # rubocop:disable Metrics/MethodLength
 class AddFieldsToUsers < ActiveRecord::Migration[5.2]
-  def change
+  def up
     rename_column :users, :name, :first_name
-    add_column :users, :last_name, :string, null: false
+    add_column :users, :last_name, :string
+    execute 'UPDATE users SET last_name = users.first_name'
+    change_column_null(:users, :last_name, false)
     add_column :users, :image_url, :string
     add_column :users, :city, :string
     add_column :users, :state, :string
@@ -14,6 +16,20 @@ class AddFieldsToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :company, :string
     add_column :users, :work_title, :string
     add_column :users, :website, :string
+  end
+
+  def down
+    rename_column :users, :first_name, :name
+    remove_column :users, :last_name, :string
+    remove_column :users, :image_url, :string
+    remove_column :users, :city, :string
+    remove_column :users, :state, :string
+    remove_column :users, :country, :string
+    remove_column :users, :newsletter, :boolean
+    remove_column :users, :bio, :text
+    remove_column :users, :company, :string
+    remove_column :users, :work_title, :string
+    remove_column :users, :website, :string
   end
 end
 # rubocop:enable Metrics/MethodLength
