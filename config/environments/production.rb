7be3c099b6f5fa94cfd968371f3a088a28a41d91
Rodrigo <roavellarm@gmail.com>
2018-09-26 17:07:37 -0300

    Fix smtp conficuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # SendGrid credentials
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
     domain: 'materiamundi.netlify.com',
