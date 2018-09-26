ad0ff55e4704947d55c907893c9cb3256f9ecbfb
Rodrigo <roavellarm@gmail.com>
2018-09-26 19:09:29 -0300

    Create message MessageMailer#email  preview

--- spec/mailers/previews/message_mailer_preview.rb
+++ spec/mailers/previews/message_mailer_preview.rb
@@ -4,6 +4,29 @@
 class MessageMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/message_mailer/email
   def email
-    MessageMailerMailer.email
+    @user = User.first
+
+    @supplier = Supplier.create!(  
+      name: 'Foo Bar',
+      description: 'Foo description',
+      website: 'http://foo',
+      email: 'foo@bar.com',
+      cnpj: '123456789',
+      company_name: 'Foo Inc',
+      municipal_subscription: 'does not apply',
+      state_subscription: '987654321',
+      phone: '5551987654321',
+      company_revenue: '100000000',
+      number_of_employees: 1000,
+      reach: 'country',
+      image_url: 'http://foo-image',
+      user: @user
+    )
+    @message = Message.create!(
+      from: @user,
+      to: @supplier,
+      text: 'Foos text'
+    )
+    MessageMailer.with(message: @message).email
   end
 end
