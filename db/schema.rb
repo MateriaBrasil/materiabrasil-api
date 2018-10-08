087e12b1c317ab317e0b3e277fd03db2cf7bbf60
Giovani <sousa_giovani@hotmail.com>
2018-10-08 11:00:41 -0300

    Ignore accents on search

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_122134) do
+ActiveRecord::Schema.define(version: 2018_10_08_132943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "addresses", force: :cascade do |t|
     t.bigint "supplier_id"
