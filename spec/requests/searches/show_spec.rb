3a34230d574d7457c3666ca87df4f4cbb64636bd
Giovani <sousa_giovani@hotmail.com>
2018-10-08 11:33:00 -0300

    Add search test material without accent and term with it

--- spec/requests/searches/show_spec.rb
+++ spec/requests/searches/show_spec.rb
@@ -86,6 +86,15 @@ describe 'GET /search', type: :request do
       it { expect(response).to have_http_status(:ok) }
       it { expect(response.body).to eq([material_with_accent].to_json) }
     end
+
+    context 'with term with accent and material without it' do
+      let(:term) { 'qúú' }
+
+      before { get '/search', params: params }
+
+      it { expect(response).to have_http_status(:ok) }
+      it { expect(response.body).to eq([material].to_json) }
+    end
   end
 
   context 'without content found' do
