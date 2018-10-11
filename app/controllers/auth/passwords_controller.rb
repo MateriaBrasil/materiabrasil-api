5c9371d9b2daff3fbcd2188c93398c4ab8dcc32f
Giovani <sousa_giovani@hotmail.com>
2018-10-11 16:48:44 -0300

    Change edit

--- app/controllers/auth/passwords_controller.rb
+++ app/controllers/auth/passwords_controller.rb
@@ -2,18 +2,46 @@
 
 module Auth
   class PasswordsController < DeviseTokenAuth::PasswordsController
-    skip_before_action :set_user_by_token, only: [:update]
-    before_action :set_user_by_reset_token, only: [:update]
+    skip_before_action :set_user_by_token, only: %i[update edit]
+    before_action :set_user_by_reset_token, only: %i[edit update]
 
     def update
       super
     end
 
+    def edit
+      # if a user is not found, return nil
+      @resource = with_reset_password_token(resource_params[:reset_password_token])
+      if @resource&.reset_password_period_valid?
+        client_id, token = @resource.create_token
+
+        # ensure that user is confirmed
+        @resource.skip_confirmation! if confirmable_enabled? && !@resource.confirmed_at
+
+        # allow user to change password once without current_password
+        @resource.allow_password_change = true if recoverable_enabled?
+
+        @resource.save!
+
+        yield @resource if block_given?
+
+        redirect_header_options = {
+          reset_password: true,
+          reset_password_token: resource_params[:reset_password_token]
+        }
+        redirect_headers = build_redirect_headers(token,
+          client_id,
+          redirect_header_options)
+        redirect_to(@resource.build_auth_url(params[:redirect_url],
+          redirect_headers))
+      else
+        render_edit_error
+      end
+    end
+
     def set_user_by_reset_token
       return if resource_params[:reset_password_token].nil?
-      @resource = with_reset_password_token(
-        resource_params[:reset_password_token]
-      )
+      @resource = with_reset_password_token(resource_params[:reset_password_token])
 
       if @resource&.reset_password_period_valid?
         bypass_sign_in(@resource)
