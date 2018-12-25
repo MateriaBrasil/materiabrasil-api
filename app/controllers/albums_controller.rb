5c2bfda1423a55017e4094bbbd2592132ce1eb9b
Glauber Ramos <glauberamos@gmail.com>
2018-12-25 19:08:12 -0200

    Add create album

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,4 +5,22 @@ class AlbumsController < ApplicationController
     album = Album.find(params[:id])
     render json: album.favorites
   end
+
+  before_action :authenticate_user!
+
+  def create
+    album = Album.create!(
+      name: album_params[:name],
+      user: current_user,
+      default: false
+    )
+
+    render status: :created, json: album
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit!
+  end
 end
---
+++ spec/requests/albums/create_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'POST /albums', type: :request do
+  let(:album) { Album.first }
+  let(:headers) { {} }
+  let(:params) do
+    {
+      name: 'foo bar',
+    }
+  end
+
+  before do
+    post '/albums', headers: headers, params: params.to_json
+  end
+
+  context 'with incorrect params' do
+    let(:params) { { foo: 'bar' } }
+
+    it { expect(response).to have_http_status(:bad_request) }
+  end
+
+  context 'without current_user' do
+    let(:headers) { { 'access-token' => nil } }
+
+    it { expect(response).to have_http_status(:unauthorized) }
+  end
+
+  context 'with current_user' do
+    it { expect(response).to have_http_status(:created) }
+    it { expect(response.body).to eq(album.to_json) }
+    it { expect(album.user).to eq(current_user) }
+  end
+end
