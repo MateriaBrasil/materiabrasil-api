6217a4866d7d8abe90da3b4a781dab5959ab299e
Glauber Ramos <glauberamos@gmail.com>
2019-01-11 13:17:39 -0200

    Update user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject(:user) do
-    User.new(first_name: 'Foo', last_name: 'Bar', email: 'foo@bar.com')
+  let(:user) do
+    User.create!(
+      email: 'bar@foo.com',
+      first_name: 'Foo',
+      last_name: 'Bar',
+      password: 'foobarfoo'
+    )
   end
 
   it { is_expected.to validate_presence_of :first_name }
@@ -14,6 +19,7 @@ RSpec.describe User, type: :model do
   it { is_expected.to have_many :comments }
   it { is_expected.to have_many :albums }
   it { is_expected.to have_many :suppliers }
+  it { is_expected.to have_many(:subscriptions) }
 
   describe '#as_json' do
     let(:json) do
@@ -40,4 +46,40 @@ RSpec.describe User, type: :model do
   end
 
   it { expect(user.tokens_has_json_column_type?).to be(false) }
+
+  describe 'subscribed?' do
+    subject { user.subscribed? }
+
+    context 'with an active subscription' do
+      before do
+        Subscription.create!(user: user, state: 'active')
+      end
+
+      it { is_expected.to be(true) }
+    end
+
+    context 'with a non-active subscription' do
+      before do
+        Subscription.create!(user: user)
+      end
+
+      it { is_expected.to be(false) }
+    end
+  end
+
+  describe 'subscription' do
+    subject { user.subscription }
+
+    context 'without subscriptions' do
+      it { is_expected.to be(nil) }
+    end
+
+    context 'with subscriptions' do
+      before do
+        Subscription.create!(user: user)
+      end
+
+      it { is_expected.to eq(user.subscriptions.order(:created_at).last) }
+    end
+  end
 end
