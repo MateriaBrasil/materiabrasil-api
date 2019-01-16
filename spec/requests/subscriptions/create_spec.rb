eaaf80dcc36e2673fd1e1734a1200996eac1185c
Glauber Ramos <glauberamos@gmail.com>
2019-01-16 13:43:25 -0200

    Improve coverage

--- spec/requests/subscriptions/create_spec.rb
+++ spec/requests/subscriptions/create_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe 'POST /subscriptions/create', type: :request do
       }.to_json
     end
 
-    it { expect(response).to have_http_status(:error) }
+    it { expect(response).to have_http_status(500) }
     it { expect(response.body).to eq(error_response) }
   end
 
@@ -38,7 +38,7 @@ RSpec.describe 'POST /subscriptions/create', type: :request do
       }.to_json
     end
 
-    it { expect(response).to have_http_status(:error) }
+    it { expect(response).to have_http_status(500) }
     it { expect(response.body).to eq(error_response) }
   end
 
@@ -46,6 +46,35 @@ RSpec.describe 'POST /subscriptions/create', type: :request do
     before do
       stub_payment_request(200)
       stub_subscription_request(status: 200, body: { id: '1' }.to_json)
+      post '/subscriptions', params: { subscription: {} }
+    end
+
+    it { expect(response).to have_http_status(:created) }
+    it { expect(response.body).to eq(subscription.reload.to_json) }
+
+    subject(:subscription) { Subscription.first }
+
+    it { expect(subscription.user).to eq(current_user) }
+    it { expect(subscription.state).to eq('pending') }
+    it { expect(subscription.payment_method).to be(nil) }
+    it { expect(subscription.activated_at).to be(nil) }
+  end
+
+  context 'when subscription is created and user as well' do
+    before do
+      stub_payment_request(200)
+      stub_subscription_request(status: 200, body: { id: '1' }.to_json)
+      stub_request(:post, 'https://api.iugu.com/v1/customers')
+        .with(
+          body: '{"email":"foo@bar.com","name":"Foo Bar"}',
+          headers: headers
+        )
+        .to_return(
+          status: 200,
+          body: { id: '1' }.to_json,
+          headers: {}
+        )
+      current_user.iugu_id = nil  
       post '/subscriptions', params: { subscription: {} }
     end
 
