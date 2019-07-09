58d41ffff0ccaac838253ba8344b9e3d19069091
Patrick Muller <patrick.e.muller@gmail.com>
2019-07-09 15:05:42 -0300

    Allow Admin to create Answer for Supplier and/or Material

--- app/policies/answer_policy.rb
+++ app/policies/answer_policy.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 class AnswerPolicy < ApplicationPolicy
+  def create?
+    owner_or_admin?
+  end
+
   def destroy?
+    owner_or_admin?
+  end
+
+  protected
+
+  def owner_or_admin?
     record_user = record.about&.user || record.about&.supplier&.user
-    record_user.id == user.id
+    record_user.id == user.id || admin?
   end
 end
