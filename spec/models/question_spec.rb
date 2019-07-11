d32bef8b761be384ad1f710a3857537b898fd82f
Patrick Muller <patrick.e.muller@gmail.com>
2019-07-11 15:32:44 -0300

    Add accepts_nested_attributes_for :options for Question
    This allow us to create the Question AND the options for this question together.
    Will be useful for the new Questionnaire's seeds

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -28,6 +28,7 @@ RSpec.describe Question, type: :model do
   end
 
   it { is_expected.to belong_to :questionnaire }
+  it { is_expected.to accept_nested_attributes_for :options }
   it { is_expected.to validate_presence_of :questionnaire }
   it { is_expected.to validate_presence_of :description }
   it { is_expected.to validate_presence_of :sorting }
