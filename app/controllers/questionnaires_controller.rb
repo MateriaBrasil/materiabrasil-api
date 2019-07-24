5aedcf5443c5c1c3aa3b064b83f7deb55e606496
Patrick Muller <patrick.e.muller@gmail.com>
2019-07-24 13:25:20 -0300

    Move filtered_questionnaires to a specific Service FilterQuestionnaires

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -2,37 +2,6 @@
 
 class QuestionnairesController < ApplicationController
   def index
-    render json: filtered_questionnaires
-  end
-
-  private
-
-  def filtered_questionnaires
-    if resource_type.present? && resource_id.present?
-      filtered_as_json
-    elsif resource_type.present? && resource_id.blank?
-      unfiltered_as_json
-    else
-      Questionnaire.all.order(:sorting).as_json
-    end
-  end
-
-  def filtered_as_json
-    # Get Supplier or Material
-    resource = resource_type.constantize.find(resource_id)
-    Questionnaire.where(about_type: resource_type)
-      .order(:sorting).as_json(resource)
-  end
-
-  def unfiltered_as_json
-    Questionnaire.where(about_type: resource_type).order(:sorting).as_json
-  end
-
-  def resource_type
-    params[:about_type]
-  end
-
-  def resource_id
-    params[:about_id]
+    render json: FilterQuestionnaires.execute(params)[:payload]
   end
 end
--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -11,16 +11,15 @@ class Questionnaire < ApplicationRecord
       about_type: about_type,
       driver: driver,
       sorting: sorting,
-      questions: filtered_questions(options)
+      questions: filtered_questions(options[:type_of_company])
     }
   end
 
   private
 
-  def filtered_questions(options)
-    if options.present?
-      size = options.type_of_company
-      questions.where.not(:"weight_for_#{size}_companies" => 0)
+  def filtered_questions(type_of_company)
+    if type_of_company.present?
+      questions.where.not(:"weight_for_#{type_of_company}_companies" => 0)
     else
       questions
     end.order(:sorting).as_json
