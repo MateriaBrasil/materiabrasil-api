39c09496141992a49caf0c9f4f1f4b8846ed371f
Eduardo Freitas <eduardorph@hotmail.com>
2019-11-04 14:11:07 -0300

    schema atualizado

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_142743) do
+ActiveRecord::Schema.define(version: 2019_11_04_143143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
